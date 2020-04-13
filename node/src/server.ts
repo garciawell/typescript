f0e204bb20bac4fcca4552e47115c43380b5422d
Wellington Garcia <garciawell@gmail.com>
2020-04-13 17:49:02 -0300

    configurando  eslint e editor config

--- node/src/server.ts
+++ node/src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello 2' })
-})
+app.get('/', (request, response) => response.json({ message: 'Hello 2' }));
 
 app.listen(3333, () => {
-  console.log(' Server started on port 3333!')
-})
+  console.log(' Server started on port 3333!');
+});
