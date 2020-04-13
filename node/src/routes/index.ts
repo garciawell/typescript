a2c3e23def0dedde5f55158898e390f3191ef0de
Wellington Garcia <garciawell@gmail.com>
2020-04-13 18:07:50 -0300

    debug vscode

--- node/src/routes/index.ts
+++ node/src/routes/index.ts
@@ -2,8 +2,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello 2 RTESTE' })
-);
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
 
 export default routes;
