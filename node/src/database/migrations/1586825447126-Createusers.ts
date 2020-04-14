fba182c1136fdf27afe7f0ef33750a4adc709206
Wellington Garcia <garciawell@gmail.com>
2020-04-13 22:17:28 -0300

    Criando relacionamento um para muitos Appointments

--- node/src/database/migrations/1586825447126-Createusers.ts
+++ node/src/database/migrations/1586825447126-Createusers.ts
@@ -8,7 +8,7 @@ export default class Createusers1586825447126 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
