27f0c82deecd066c505ae351dc67c5bfd8bf62aa
Wellington Garcia <garciawell@gmail.com>
2020-04-24 19:15:57 -0300

    componentizando pagina de login

--- react-native/gobarber/src/pages/SignIn/SignIn.tsx
+++ react-native/gobarber/src/pages/SignIn/SignIn.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Image } from 'react-native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 import logoImg from '../../assets/img/logo.png';
 
@@ -8,6 +11,11 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
