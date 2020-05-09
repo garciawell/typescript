69e2c82489522dd93c941aeef84a3790bf0ff2f3
Wellington Garcia <garciawell@gmail.com>
2020-05-09 12:44:38 -0300

    Alterando provider upload

--- node/gobarber/src/modules/users/services/UpdateUserAvatarService.ts
+++ node/gobarber/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,8 +1,6 @@
-import path from 'path';
-import fs from 'fs';
-import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUserRepository';
 
@@ -15,7 +13,10 @@ interface IRequest {
 class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) {}
 
   public async execute({ user_id, avatarFileName }: IRequest): Promise<User> {
@@ -26,16 +27,11 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
+    const filename = await this.storageProvider.saveFile(avatarFileName);
 
-    user.avatar = avatarFileName;
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
--- node/gobarber/src/shared/container/index.ts
+++ node/gobarber/src/shared/container/index.ts
@@ -1,5 +1,6 @@
 import { container } from 'tsyringe';
 import '@modules/users/providers';
+import './providers';
 import IAppointmentsRepository from '@modules/appointsments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointsments/infra/typeorm/repositories/AppointmentsRepository';
 
