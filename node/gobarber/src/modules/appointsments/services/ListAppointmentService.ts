13f473598aacd86a0784c596e62613f5d22be35c
Wellington Garcia <garciawell@gmail.com>
2020-05-26 22:05:15 -0300

    Cache Lista Providers

--- node/gobarber/src/modules/appointsments/services/ListAppointmentService.ts
+++ node/gobarber/src/modules/appointsments/services/ListAppointmentService.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 import IAppointmentsRepository from '@modules/appointsments/repositories/IAppointmentsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 interface IRequest {
@@ -13,7 +14,10 @@ interface IRequest {
 class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
-    private appointmentsRepository: IAppointmentsRepository
+    private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider
   ) {}
 
   public async execute({
@@ -22,12 +26,18 @@ class ListProviderAppointmentsService {
     year,
     month,
   }: IRequest): Promise<Appointment[]> {
+    const cacheData = await this.cacheProvider.recover('qweqwe');
+
+    console.log('cacheData', cacheData);
+
     const appointsments = await this.appointmentsRepository.findAllInDay({
       provider_id,
       day,
       year,
       month,
     });
+
+    // await this.cacheProvider.save('qweqwe', 'qwqeqweqeeq');
 
     return appointsments;
   }
